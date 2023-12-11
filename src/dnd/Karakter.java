95cf0ecc5c4797269375a5819ddede11fefde776
Nguyen Duc Duy <nguyen.d.duy@dk.akkszalezi.hu>
2023-12-11 13:26:14 +0100

    statok generalasanak letrehozasa

--- src/dnd/Karakter.java
+++ src/dnd/Karakter.java
@@ -1,5 +1,15 @@
 package dnd;
 
+import java.util.Random;
+
 public class Karakter {
-    
+
+    private int eletero, ugyesseg, szerencse;
+    private Random rnd;
+
+    public Karakter(int eletero, int ugyesseg, int szerencse) {
+        this.eletero = (rnd.nextInt(1, 7) * 2) + 12;
+        this.ugyesseg = rnd.nextInt(1, 7) + 6;
+        this.szerencse = rnd.nextInt(1, 7) + 6;
+    }
 }
