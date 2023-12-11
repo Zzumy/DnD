13cd664033e04b27a4397ad23ca8c0271958a5e6
Nguyen Duc Duy <nguyen.d.duy@dk.akkszalezi.hu>
2023-12-11 13:52:18 +0100

    Targy osztaly letrehozva

---
+++ src/dnd/Targy.java
@@ -0,0 +1,16 @@
+package dnd;
+
+import java.util.UUID;
+
+public class Targy {
+
+    private String nev, leiras;
+    UUID id;
+
+    public Targy(String nev, String leiras, UUID id) {
+        this.nev = nev;
+        this.leiras = leiras;
+        this.id = id;
+    }
+
+}
