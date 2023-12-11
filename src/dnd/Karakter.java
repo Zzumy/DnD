58bfc5c0e64723b33c4b9fb15c4d88d71842f04f
Nguyen Duc Duy <nguyen.d.duy@dk.akkszalezi.hu>
2023-12-11 13:46:18 +0100

    Targy osztaly letrehozva es felvesz metodus

--- src/dnd/Karakter.java
+++ src/dnd/Karakter.java
@@ -7,11 +7,21 @@ public class Karakter {
 
     private Random rnd;
     private int eletero, ugyesseg, szerencse;
-    ArrayList<String> felszereles;
+    ArrayList<Targy> felszereles;
 
     public Karakter(int eletero, int ugyesseg, int szerencse) {
         this.eletero = (rnd.nextInt(1, 7) * 2) + 12;
         this.ugyesseg = rnd.nextInt(1, 7) + 6;
         this.szerencse = rnd.nextInt(1, 7) + 6;
+    }
+
+    public void felvesz(Targy targy) {
+        felszereles.add(targy);
+    }
+
+    public void felveszTobb(Targy targy, int mennyi) {
+        for (int ix = 0; ix < mennyi; ix++) {
+            felszereles.add(targy);
+        }
     }
 }
